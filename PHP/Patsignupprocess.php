238c10eba752297705453a60113850678cb48fa9
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-10-18 11:41:21 +0600

    SIGN UP DONE

--- PHP/Patsignupprocess.php
+++ PHP/Patsignupprocess.php
@@ -2,9 +2,10 @@
 echo ' <h1><center>Hello Patient</center></h1>';
 echo ' <h1><center>Welcome to Personal Health Application</center></h1>';
 echo ' <h1><center>Give your information</center></h1>';
+$usernameInDB = $mailInDB = "";
 
 if (isset($_POST["submit"])) {
-   
+
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
 
@@ -18,22 +19,44 @@ if (isset($_POST["submit"])) {
     $user_email = $_POST['user_email'];
 
 
-    $conn = mysqli_connect('localhost','root','','phawa');
-    $sql="INSERT INTO patienttbl(pid,ptname,ptphone,ptgender,ptage,ptbgrp,ptusername,ptpass,ptuseremail) VALUES ('0','$name','$phoneno','$gender','$age','$Bgrp','$user_name','$user_pass','$user_email')";
-    
-    if(mysqli_query($conn,$sql))
-    {
-        echo 'I am working';
-        session_start();
-        $_SESSION["user_name"]=$_POST["user_name"];
-        echo "Signup Done Bro !";
-        mysqli_close($conn);
+    $conn = mysqli_connect('localhost', 'root', '', 'phawa');
+    $sql = "INSERT INTO patienttbl(pid,ptname,ptphone,ptgender,ptage,ptbgrp,ptusername,ptpass,ptuseremail) VALUES ('0','$name','$phoneno','$gender','$age','$Bgrp','$user_name','$user_pass','$user_email')";
 
-    }
-    else
-    {
-        echo "Signup is not Done Bro !";
-    }
+    if (isset($fname) && isset($lname) && isset($phoneno) && isset($gender) && isset($age) && isset($Bgrp) && isset($user_name) && isset($user_pass) && isset($user_email)) {
 
+        //  $user_name = mysqli_real_escape_string($conn, $_POST['$user_name']);
+
+        $query = "SELECT ptusername FROM patienttbl WHERE ptusername='$user_name';";
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $usernameInDB = $row['ptusername'];
+        }
+
+
+        $query = "SELECT ptuseremail FROM patienttbl WHERE ptuseremail='$user_email'";
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $mailInDB = $row['ptuseremail'];
+        }
+
+
+        if ($usernameInDB == $user_name) {
+
+            echo '<script>alert("User Name already available!,Try different User Name!")</script>';
+            mysqli_close($conn);
+        } else  if ($mailInDB == $user_email) {
+            echo '<script>alert("Mail address already exist !,Try different mail address!")</script>';
+            mysqli_close($conn);
+        } else {
+            if (mysqli_query($conn, $sql)) {
+                echo 'I am working';
+                session_start();
+                $_SESSION["user_name"] = $_POST["user_name"];
+                echo "Signup Done Bro !";
+                mysqli_close($conn);
+            } else {
+                echo "Signup is not Done Bro !";
+            }
+        }
+    }
 }
-?>
