ca016d75d41d4a210745e599cc20e38d88b09636
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-10-18 20:13:49 +0600

    Login session done

--- PHP/patprofile.php
+++ PHP/patprofile.php
@@ -1,19 +1,29 @@
 <?php
 echo ' <h1><center>Hello Patients profile </center></h1>';
 echo ' <h1><center>Welcome to Personal Health Application</center></h1>';
+session_start();
 
+
+if (!isset($_SESSION["user_name"])) {
+    header("refresh: 1; url=patindex.php");
+    exit();
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<a href="Patlogout.php">Logout</a>
+    <p><?php echo $_SESSION["user_name"] ?></p>
+    <a href="Patlogout.php">Logout</a>
 </body>
+
 </html>
