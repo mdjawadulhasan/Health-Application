1816f24787bde8c4679086c70089614fb42cfef4
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-11-06 07:03:29 +0600

    SIdebar Done

--- PHP/Patient/PatientHome.php
+++ PHP/Patient/PatientHome.php
@@ -3,3 +3,4 @@
 <?php require_once './includes/sidebar.php'; ?>
 
 <?php require_once './includes/footer.php'; ?>
+
--- PHP/Patient/includes/sidebar.php
+++ PHP/Patient/includes/sidebar.php
@@ -1,57 +1,40 @@
 <div class="sidediv">
-      <div class="btn">
-        <span class="fas fa-bars"></span>
-      </div>
-      <nav class="sidebar">
-        <div class="text">Side Menu</div>
-        <ul>
-          <li class="active">
-            <a href="#"><i class="fas fa-home"></i> Dashboard</a>
-          </li>
-          <li>
-            <a href="#" class="feat-btn"
-              >Features
-              <span class="fas fa-caret-down first"></span>
-            </a>
-            <ul class="feat-show">
-              <li><a href="#">Pages</a></li>
-              <li><a href="#">Elements</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#" class="serv-btn"
-              >Services
-              <span class="fas fa-caret-down second"></span>
-            </a>
-            <ul class="serv-show">
-              <li><a href="#">App Design</a></li>
-              <li><a href="#">Web Design</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">Overview</a></li>
-          <li><a href="#">Shortcuts</a></li>
-          <li><a href="#">Feedback</a></li>
+  <div class="btn">
+    <span class="fas fa-bars"></span>
+  </div>
+  <nav class="sidebar">
+    <div class="text">Menu</div>
+    <ul>
+      <li>
+        <a href="#" class="feat-btn"><i class="far fa-calendar-check"></i>  Appointment
+          <span class="fas fa-caret-down first"></span>
+        </a>
+        <ul class="feat-show">
+          <li><a href="../../../../PHAWA/PHP/Patient/Bookappointment.php">Book Appointment</a></li>
+          <li><a href="../../../../PHAWA/PHP/Patient/Viewappointment.php">View Appointment</a></li>
         </ul>
-      </nav>
-    </div>
+      </li>
+      <li><a href="../../../../PHAWA/PHP/Patient/Seepresc.php"><i class="fas fa-file-medical"></i>  Prescriptions </a></li>
+      <li><a href="../../../../PHAWA/PHP/Patient/Donorlist.php"> <i class="fas fa-hand-holding-water"></i>  Donor Lists </a></li>
+      <li><a href="#"><i class="fas fa-file-medical-alt"> </i>  Vaccination History</a></li>
+    </ul>
+  </nav>
+</div>
 
-    <script>
-      $(".btn").click(function () {
-        $(this).toggleClass("click");
-        $(".sidebar").toggleClass("show");
-      });
-      $(".feat-btn").click(function () {
-        $("nav ul .feat-show").toggleClass("show");
-        $("nav ul .first").toggleClass("rotate");
-      });
-      $(".serv-btn").click(function () {
-        $("nav ul .serv-show").toggleClass("show1");
-        $("nav ul .second").toggleClass("rotate");
-      });
-      $("nav ul li").click(function () {
-        $(this).addClass("active").siblings().removeClass("active");
-      });
-    </script>
-    
- 
+<script>
+  $(".btn").click(function() {
+    $(this).toggleClass("click");
+    $(".sidebar").toggleClass("show");
+  });
+  $(".feat-btn").click(function() {
+    $("nav ul .feat-show").toggleClass("show");
+    $("nav ul .first").toggleClass("rotate");
+  });
+  $(".serv-btn").click(function() {
+    $("nav ul .serv-show").toggleClass("show1");
+    $("nav ul .second").toggleClass("rotate");
+  });
+  $("nav ul li").click(function() {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+</script>
