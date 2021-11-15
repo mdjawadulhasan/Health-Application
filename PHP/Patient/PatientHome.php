47ecd3655abb873dff77635b3d689276b6343a00
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-11-16 00:40:46 +0600

    Home Ongoing 2.0

--- PHP/Patient/PatientHome.php
+++ PHP/Patient/PatientHome.php
@@ -14,60 +14,61 @@ require_once './includes/sidebar.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/Homestyle.css">
 </head>
+
 <body>
-<section class="services" id="services">
-    
-<div class="box-container">
+    <section class="services" id="services">
 
-    <div class="box b1">
-        <i class="fas fa-notes-medical"></i>
-        <h3>free checkups</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
-        <a href="#" class="btngo"> learn more <span class="fas fa-chevron-right"></span> </a>
-    </div>
+        <div class="box-container">
+        
+            <div class="box b1">
+                <a href="Sleepdata.php" class="fas fa-bed"></a>
+                <h3>Sleep</h3>
+                <p>Update Your Regular Sleep Data.</p>
 
-    <div class="box b2">
-        <i class="fas fa-ambulance"></i>
-        <h3>24/7 ambulance</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
-        <a href="#" class="btngo"> learn more <span class="fas fa-chevron-right"></span> </a>
-    </div>
+            </div>
 
-    <div class="box">
-        <i class="fas fa-user-md"></i>
-        <h3>expert doctors</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
-        <a href="#" class="btngo"> learn more <span class="fas fa-chevron-right"></span> </a>
-    </div>
+            <div class="box b2">
+                <a href="https://www.google.com/" class="fas fa-ambulance"></a>
+                <h3>24/7 ambulance</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
 
-    <div class="box">
-        <i class="fas fa-pills"></i>
-        <h3>medicines</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
-        <a href="#" class="btngo"> learn more <span class="fas fa-chevron-right"></span> </a>
-    </div>
+            </div>
 
-    <div class="box">
-        <i class="fas fa-procedures"></i>
-        <h3>bed facility</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
-        <a href="#" class="btngo"> learn more <span class="fas fa-chevron-right"></span> </a>
-    </div>
+            <div class="box">
+                <a href="https://www.google.com/" class="fas fa-user-md"></a>
+                <h3>expert doctors</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
 
-    <div class="box">
-        <i class="fas fa-heartbeat"></i>
-        <h3>total care</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
-        <a href="#" class="btngo"> learn more <span class="fas fa-chevron-right"></span> </a>
-    </div>
+            </div>
 
-</div>
-</section>
+            <div class="box">
+                <a href="https://www.google.com/" class="fas fa-pills"></a>
+                <h3>medicines</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+
+            </div>
+
+            <div class="box">
+                <a href="https://www.google.com/" class="fas fa-procedures"></a>
+                <h3>bed facility</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+
+            </div>
+
+            <div class="box">
+                <a href="https://www.google.com/" class="fas fa-heartbeat"></a>
+                <h3>Total care</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+
+            </div>
+
+        </div>
+    </section>
 </body>
-
