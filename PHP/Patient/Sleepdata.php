caf216026d079a86c42fac49428c63f67ef0e580
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-11-18 15:38:47 +0600

    DB CON 2.0

--- PHP/Patient/Sleepdata.php
+++ PHP/Patient/Sleepdata.php
@@ -90,6 +90,7 @@ require_once './includes/sidebar.php';
     var counter = document.querySelector('#fnumber').value;
 
     document.querySelector('#fnumber').value = counter;
+
     function check() {
         if (counter > 5) {
             document.querySelector('.counterbox').style.color = "#16a085";
@@ -134,17 +135,38 @@ require_once './includes/sidebar.php';
 
 if (isset($_POST["Setted"])) {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $count=0;
 
         $countval = $_POST['Count'];
         $user_name = $_SESSION["user_name"];
-        $Todaydt=date('Y-m-d');
-         
-
+        $Todaydt = date('Y-m-d');
         $conn = mysqli_connect('localhost', 'root', '', 'phawa');
-        $sql="INSERT INTO sleepdatatbl(sleepdataid,username,crnt_date,hrcounter) VALUES ('0','$user_name',' $Todaydt','$countval')";
+
+
+        $query = "SELECT *FROM sleepdatatbl where crnt_date='$Todaydt' and username='$user_name'";
+        $result = mysqli_query($conn, $query);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+        $sql="";
+       
+
+
+        if ($count >= 1) 
+        {
+            $sql = "UPDATE sleepdatatbl SET hrcounter=' $countval' where crnt_date='$Todaydt' and username='$user_name'";
+        } 
+        else {
+            $sql = "INSERT INTO sleepdatatbl(sleepdataid,username,crnt_date,hrcounter) VALUES ('0','$user_name',' $Todaydt','$countval')";
+            
+        }
+
         if (mysqli_query($conn, $sql)) {
             mysqli_close($conn);
-        } 
+        }
+        else
+        {
+            echo '<script>alert("Try Again")</script>';
+        }
     }
 }
 
