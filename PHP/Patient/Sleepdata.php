cf2e5d74a48fa6bef8cfe0ac71720044a20bd879
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-11-18 16:04:58 +0600

    DB doing

--- PHP/Patient/Sleepdata.php
+++ PHP/Patient/Sleepdata.php
@@ -10,6 +10,16 @@ if (!isset($_SESSION["user_name"])) {
 $title = 'Sleep Record';
 require_once './includes/header.php';
 require_once './includes/sidebar.php';
+$conn = mysqli_connect('localhost', 'root', '', 'phawa');
+
+$defaultvalue=0;
+$user_name = $_SESSION["user_name"];
+$Todaydt = date('Y-m-d');
+$query = "SELECT *FROM sleepdatatbl where crnt_date='$Todaydt' and username='$user_name'";
+$result = mysqli_query($conn, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+    $defaultvalue = $row['hrcounter'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -41,7 +51,7 @@ require_once './includes/sidebar.php';
                         echo $_POST['Count'];
 
                     else
-                        echo '0';
+                        echo $defaultvalue;
                     ?>
                 </span> Hours
             </div>
@@ -55,7 +65,7 @@ require_once './includes/sidebar.php';
                                                                                 echo $_POST['Count'];
 
                                                                             else
-                                                                                echo '0';
+                                                                                echo $defaultvalue;
                                                                             ?>" required>
                     <input type="submit" value="SET" name="Setted" class="btnset">
                 </form>
@@ -135,36 +145,31 @@ require_once './includes/sidebar.php';
 
 if (isset($_POST["Setted"])) {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $count=0;
+        $count = 0;
 
         $countval = $_POST['Count'];
         $user_name = $_SESSION["user_name"];
-        $Todaydt = date('Y-m-d');
-        $conn = mysqli_connect('localhost', 'root', '', 'phawa');
+        // $Todaydt = date('Y-m-d');
+        // $conn = mysqli_connect('localhost', 'root', '', 'phawa');
 
 
         $query = "SELECT *FROM sleepdatatbl where crnt_date='$Todaydt' and username='$user_name'";
         $result = mysqli_query($conn, $query);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
-        $sql="";
-       
+        $sql = "";
 
 
-        if ($count >= 1) 
-        {
+
+        if ($count >= 1) {
             $sql = "UPDATE sleepdatatbl SET hrcounter=' $countval' where crnt_date='$Todaydt' and username='$user_name'";
-        } 
-        else {
+        } else {
             $sql = "INSERT INTO sleepdatatbl(sleepdataid,username,crnt_date,hrcounter) VALUES ('0','$user_name',' $Todaydt','$countval')";
-            
         }
 
         if (mysqli_query($conn, $sql)) {
             mysqli_close($conn);
-        }
-        else
-        {
+        } else {
             echo '<script>alert("Try Again")</script>';
         }
     }
