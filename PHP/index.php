4cd2a42c64be2cf41ed1bc2c97db9a16293c0053
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-11-28 02:26:12 +0600

    HOME PAGE 1.0

--- PHP/index.php
+++ PHP/index.php
@@ -57,6 +57,51 @@
     </div>
   </section>
 
+  <div class="servicecontainer">
+    <div class="services s1">
+      <i class="far fa-calendar-check"></i>
+      <br><br><br>
+      <h1>Book an Appointment</h1>
+      <br>
+      <p>Find doctors with their department name and set an appointment</p>
+    </div>
+    <div class="services s2">
+      <i class="fas fa-file-prescription"></i>
+      <br><br><br>
+      <h1>Online Prescription</h1>
+      <br>
+      <p> View All the digital prescriptions and keep them for a lifetime.</p>
+
+    </div>
+    <div class="services s3">
+      <i class="fas fa-heart"></i>
+      <br><br><br>
+      <h1>Health Care</h1>
+      <br>
+      <p> Keep updated with all the personal data and keep them for a long time</p>
+    </div>
+    <div class="services s4">
+      <i class="fas fa-user-nurse"></i>
+      <br><br><br>
+      <h1>Blood Donor</h1>
+      <br>
+      <p> See the nearby blood donors and contact with them.</p>
+    </div>
+  </div>
+
+  <div class="covidinfocontainer">
+    <div class="coimg">
+      <img src="../Images/Covidimg.svg" alt="">
+    </div>
+    <div class="coninfo">
+      <p>Know more About Covid-19 Outbreak !</p>
+      <h2>Check real time covid data</h2>
+      <br>
+      <h2>Test covid-19 by providing your symptoms</h2>
+
+    </div>
+  </div>
+
 
   <script>
     var count = 0;
@@ -65,15 +110,15 @@
       if (count % 2 == 0) {
         count++;
         document.querySelector(".home").style.backgroundColor = "#5b6361";
-        document.querySelector("h3").style.color="white";
-        document.querySelector("p").style.color="white";
+        document.querySelector("h3").style.color = "white";
+        document.querySelector("p").style.color = "white";
       } else {
         count++;
-        document.querySelector(".home").style.backgroundColor = "white";
-        document.querySelector("h3").style.color="black";
-        document.querySelector("p").style.color="#777";
+        document.querySelector(".home").style.backgroundColor = "#fafafa";
+        document.querySelector("h3").style.color = "black";
+        document.querySelector("p").style.color = "#777";
       }
-      
+
     }
   </script>
 </body>
