51abc8da3d0b0246ed0286c3bcdc6eda361a15c8
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-11-28 00:30:51 +0600

    Iframe UPdated

--- PHP/Patient/Covidtest.php
+++ PHP/Patient/Covidtest.php
@@ -17,9 +17,24 @@ require_once './includes/sidebar.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .framediv {
+            margin-top: -180px;
+            margin-right: 0px;
+            
+        }
+        *{
+            overflow-y: hidden;
+        }
+        
+    </style>
+
 </head>
+
 <body>
-<iframe src="https://livecoronatest.com/chat.php?city=Unnamed%20Road&lat=23.820264&lng=90.417367&addr_dist=Unknown&addr_div=Unknown" width="100%" height="1000px" ></iframe>
+    <div class="framediv">
+        <iframe src="https://livecoronatest.com/chat.php?city=Unnamed%20Road&lat=23.820264&lng=90.417367&addr_dist=Unknown&addr_div=Unknown" width="100%" height="1000px"></iframe>
+    </div>
 </body>
 
 </html>
--- PHP/Patient/signup.php
+++ PHP/Patient/signup.php
@@ -17,7 +17,6 @@ if (session_status() >= 0) {
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!------ Include the above in your HEAD tag ---------->
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -36,7 +35,7 @@ if (session_status() >= 0) {
                 <h4>To</h4>
                 <h4>Health Care System</h4>
                 <p>Provide Your information and join Now!</p>
-                <input type="submit" class="loginbtn" name="" value="Login" onclick="location.href='Patsignin.php';"/><br />
+                <input type="submit" class="loginbtn" name="" value="Login" onclick="location.href='Patsignin.php';" /><br />
             </div>
             <div class="col-md-9 register-right">
                 <div class="tab-content" id="myTabContent">
