aebadb9007841cea6bb8a1778e4dbc30b07f9b5d
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-11-29 22:07:27 +0600

    Warning

--- PHP/Patient/PatientHome.php
+++ PHP/Patient/PatientHome.php
@@ -20,9 +20,21 @@ require_once './includes/sidebar.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/Homestyle.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 </head>
 
 <body>
+
+    <div class="alert hide">
+        <span class="fas fa-exclamation-circle"></span>
+        <span class="msg">Warning: Update Your Health Data!</span>
+        <div class="close-btn">
+            <span class="fas fa-times"></span>
+        </div>
+    </div>
+
     <section class="services" id="services">
 
         <div class="box-container">
@@ -47,7 +59,7 @@ require_once './includes/sidebar.php';
                 <p>Update Your Body Weight</p>
 
             </div>
-           
+
             <div class="box b4">
                 <a href="Exercisedata.php" class="fas fa-running"></a>
                 <h3>Exercise</h3>
@@ -73,4 +85,59 @@ require_once './includes/sidebar.php';
 
         </div>
     </section>
+
+    <script>
+        function show() {
+            $('.alert').addClass("show");
+            $('.alert').removeClass("hide");
+            $('.alert').addClass("showAlert");
+        }
+        $('.close-btn').click(function() {
+            $('.alert').removeClass("show");
+            $('.alert').addClass("hide");
+        });
+    </script>
 </body>
+
+
+<?php
+$Todaydt = date('Y-m-d');
+$user_name = $_SESSION["user_name"];
+$conn = mysqli_connect('localhost', 'root', '', 'phawa');
+
+$query = "SELECT *FROM sleepdatatbl where crnt_date='$Todaydt' and username='$user_name'";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count1 = mysqli_num_rows($result);
+
+$query = "SELECT *FROM heartratedatatbl where crnt_date='$Todaydt' and username='$user_name'";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count2 = mysqli_num_rows($result);
+
+$query = "SELECT *FROM weightdatatbl where crnt_date='$Todaydt' and username='$user_name'";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count3 = mysqli_num_rows($result);
+
+$query = "SELECT *FROM excdatatbl where crnt_date='$Todaydt' and username='$user_name'";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count4 = mysqli_num_rows($result);
+
+$query = "SELECT *FROM waterdatatbl where crnt_date='$Todaydt' and username='$user_name'";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count5 = mysqli_num_rows($result);
+
+$count=(min($count1, $count2, $count3, $count4, $count5));
+
+
+if ($count <= 0) {
+
+    echo '<script type="text/javascript">',
+    'show();',
+    '</script>';
+}
+
+?>
