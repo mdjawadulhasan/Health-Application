69bdb05fce0ac5411938d332c70f37577234e738
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-12-05 12:09:43 +0600

    Covid Map

--- PHP/Patient/covidinfo.php
+++ PHP/Patient/covidinfo.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION["user_name"])) {
-    header("refresh: 0; url=Patsignin.php");
-    exit();
+  header("refresh: 0; url=Patsignin.php");
+  exit();
 }
 $title = 'Covid-19';
 require_once './includes/header.php';
@@ -14,44 +14,80 @@ require_once './includes/sidebar.php';
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Covid Datas</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../Patient/css/stylecovid.css"/>
-  </head>
-  <body>
-    <div class="container">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Covid Datas</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../Patient/css/stylecovid.css" />
+</head>
+
+<body>
+  <div class="container">
     <h2><img src="" id="flag"></h2>
     <h2><span id="country"></span> </h2>
-      <span><h2>COVID -19 Dashboard</h2></span>
-      <div class="board">
-        <div class="card a"><i class="fa fa-tachometer"></i><h5>Active Cases</h5><span id="active"></span></div>
-        <div class="card ca"><i class="fa fa-th-list"></i><h5>Total Cases</h5><span id="cases"></span></div>
-        <div class="card cr"><i class="fas fa-exclamation-triangle"></i><h5>Critical Cases</h5><span id="critical"></span></div>
-        <div class="card d"><i class="fa fa-times"></i><h5>Total Deaths</h5><span id="death"></span></div>
-        <div class="card r"><i class="fas fa-user-check"></i><h5>Recovered Cases</h5><span id="recovered"></span></div>
-        <div class="card t"><i class="fas fa-vials"></i><h5>Total Testes Done</h5><span id="tests"></span></div>
+    <span>
+      <h2>COVID -19 Dashboard</h2>
+    </span>
+    <div class="board">
+      <div class="card a"><i class="fa fa-tachometer"></i>
+        <h5>Active Cases</h5><span id="active"></span>
+      </div>
+      <div class="card ca"><i class="fa fa-th-list"></i>
+        <h5>Total Cases</h5><span id="cases"></span>
+      </div>
+      <div class="card cr"><i class="fas fa-exclamation-triangle"></i>
+        <h5>Critical Cases</h5><span id="critical"></span>
+      </div>
+      <div class="card d"><i class="fa fa-times"></i>
+        <h5>Total Deaths</h5><span id="death"></span>
+      </div>
+      <div class="card r"><i class="fas fa-user-check"></i>
+        <h5>Recovered Cases</h5><span id="recovered"></span>
+      </div>
+      <div class="card t"><i class="fas fa-vials"></i>
+        <h5>Total Testes Done</h5><span id="tests"></span>
       </div>
     </div>
-  </body>
+  </div>
+  <button class="sbtn" onclick="ShowCovid()">Show Map</button>
+ 
+  <div id="covidmap">
+    <iframe src="https://ourworldindata.org/grapher/total-cases-covid-19?tab=map" width="100%" height="600px"></iframe>
+  </div>
+
+  
+  
+
+</body>
+
 </html>
 <script type="text/javascript">
+  fetch('https://corona.lmao.ninja/v2/countries/Bangladesh')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      id = data.country;
+      document.getElementById("active").innerHTML = data.active.toLocaleString();
+      document.getElementById("cases").innerHTML = data.cases.toLocaleString();
+      document.getElementById("critical").innerHTML = data.critical.toLocaleString();
+      document.getElementById("death").innerHTML = data.deaths.toLocaleString();
+      document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
+      document.getElementById("tests").innerHTML = data.tests.toLocaleString();
+      document.getElementById("flag").src = data.countryInfo.flag;
+    });
 
-fetch('https://corona.lmao.ninja/v2/countries/Bangladesh')
-.then((response) => {
-  return response.json();
-})
-.then((data) => {
-  console.log(data);
-  document.getElementById("country").innerHTML = data.country;
-  document.getElementById("active").innerHTML = data.active.toLocaleString();
-  document.getElementById("cases").innerHTML = data.cases.toLocaleString();
-  document.getElementById("critical").innerHTML = data.critical.toLocaleString();
-  document.getElementById("death").innerHTML = data.deaths.toLocaleString();
-  document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
-  document.getElementById("tests").innerHTML = data.tests.toLocaleString();
-  document.getElementById("flag").src = data.countryInfo.flag;
-});
+    function ShowCovid() {
+    var x = document.getElementById("covidmap");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
 </script>
--- PHP/Patient/includes/header.php
+++ PHP/Patient/includes/header.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
 
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-   
+
     <link rel="stylesheet" href="./css/style.css">
 
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
-    
+
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="sweetalert2.min.js"></script>
     <link rel="stylesheet" href="sweetalert2.min.css">
-    
+
     <link rel="icon" href="../../Images/ticon.svg" type="image/icon type">
-    
+
 
 </head>
 
@@ -33,9 +39,8 @@
     <nav class="navbar">
         <a href="PatientHome.php">Home</a>
         <a href="Patprofile.php">Profile</a>
-        <a href="Patlogout.php">Log out</a> 
+        <a href="Patlogout.php">Log out</a>
     </nav>
 </header>
 
 <body>
-
