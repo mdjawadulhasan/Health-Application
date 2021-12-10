af56411623727d41e5a8bb84d8d0c291b93e6506
mdjawadulhasan <20-41958-1@student.aiub.edu>
2021-12-10 12:48:21 +0600

    Ambulance Added

---
+++ PHP/Patient/Ambulancebookprcs.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+if (!isset($_SESSION["user_name"])) {
+    header("refresh: 0; url=Patsignin.php");
+    exit();
+}
+
+?>
+
+<?php
+
+if (isset($_POST["submit"])) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $user_name = $_SESSION["user_name"];
+        $pickup = $_POST['padd'];
+        $destination = $_POST['dadd'];
+
+
+        $conn = mysqli_connect('localhost', 'root', '', 'phawa');
+        $sql = "INSERT INTO ambulancebooktbl(bookingid,username,pickupadd,Destinationadd) VALUES ('0','$user_name','$pickup', '$destination')";
+        if (mysqli_query($conn, $sql)) {
+            header("location:Ambulancecall.php");
+            mysqli_close($conn);
+        } else {
+            echo '<script>alert("Try Again!")</script>';
+        }
+    }
+}
+
+?>
+
+
+<?php
+
+if (isset($_POST["cancel"])) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $user_name = $_SESSION["user_name"];
+       
+
+
+        $conn = mysqli_connect('localhost', 'root', '', 'phawa');
+        $sql = "DELETE FROM ambulancebooktbl where username='$user_name'";
+        if (mysqli_query($conn, $sql)) {
+            header("location:Ambulancecall.php");
+            mysqli_close($conn);
+        } else {
+            echo '<script>alert("Try Again!")</script>';
+        }
+    }
+}
+
+?>
--- PHP/Patient/Ambulancecall.php
+++ PHP/Patient/Ambulancecall.php
@@ -10,9 +10,26 @@ require_once './includes/header.php';
 require_once './includes/sidebar.php';
 ?>
 
+<?php
+$count=0;
+$user_name = $_SESSION["user_name"];
+$conn = mysqli_connect('localhost', 'root', '', 'phawa');
+$query = "SELECT * from ambulancebooktbl where username='$user_name'";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
+
+
+?>
+
+
+
 <section>
     <div class="amb">
-       
+        <div class="ambulanceimg">
+            <img src="../../Images/amb.svg" id="imgid">
+            
+        </div>
         <div class="mapouter">
             <div class="gmap_canvas"><iframe width="539" height="420" id="gmap_canvas" src="https://maps.google.com/maps?q=Ambulance&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br>
                 <style>
@@ -33,25 +50,47 @@ require_once './includes/sidebar.php';
                 </style>
             </div>
         </div>
-        <div class="ambulanceimg">
-            <img src="../../Images/amb.svg">
-        </div>
+
     </div>
     <div class="bookamb">
-        <h2>Provide Details</h2>
-        <br>
-        <form>
+        <h2>Provide Details</h2> <h4 id="bookedtext"></h4>
+        <h3 id="stattext"></h3>
+        <form action="Ambulancebookprcs.php" method="post">
             <div class="form-group">
-                <label for="formGroupExampleInput">Example label</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+                <label for="formGroupExampleInput">Pickup Address</label>
+                <input type="text" name="padd" class="form-control" id="formGroupExampleInput" required>
             </div>
             <div class="form-group">
-                <label for="formGroupExampleInput2">Another label</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                <label for="formGroupExampleInput2">Destination Address</label>
+                <input type="text" name="dadd" class="form-control" id="formGroupExampleInput2" required>
             </div>
-            <input type="submit" name="submit" value="book now" class="btnset">
+            <button type="submit" id="bookambbtn" name="submit"  class="btnset">Book now</button>
+            
+            
+        </form>
+        <form action="Ambulancebookprcs.php" method="post">
+        <button type="submit" id="cancelbbtn" name="cancel"  class="btnset">Cancel Booking  </button>
         </form>
     </div>
 
-
+    <script type="text/javascript">
+        var val = "<?php echo"$count"?>";
+        if (val>=1){
+    
+            document.getElementById("bookedtext").innerHTML="-Already Boked-";
+            document.getElementById("imgid").src="../../Images/Runningamb.svg";
+            document.getElementById("bookambbtn").style.visibility="hidden";
+            document.getElementById("stattext").innerHTML="An ambulance is on your way"
+        }
+        else
+        {
+            document.getElementById("cancelbbtn").style.visibility="hidden";
+            document.getElementById("stattext").style.visibility="hidden";
+            document.getElementById("bookedtext").style.visibility="hidden";
+        }
+    </script>
 </section>
+
+
+
+
